ea5d2946456e9f502b3016e36b7189098f3836c8
Joshua Hartshorn <Pops@Joshuas-MacBook-Air-2.local>
2020-08-02 00:09:32 -0700

    Updated code for front end

---
+++ client/src/utils/queries.js
@@ -0,0 +1,39 @@
+import gql from 'graphql-tag';
+
+export const QUERY_THOUGHTS = gql`
+  query thoughts($username: String) {
+    thoughts(username: $username) {
+      _id
+      thoughtText
+      createdAt
+      username
+      reactionCount
+      reactions {
+        _id
+        createdAt
+        username
+        reactionBody
+      }
+    }
+  }
+`;
+
+export const GET_ME = gql`
+{
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        _id
+        bookId
+        authors
+        image
+        link
+        title
+        description
+      }
+    }
+  }
+`;
