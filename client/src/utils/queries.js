60887c2f4fb87913644f4f5b5bd45361cbdf12d0
Joshua Hartshorn <Pops@Joshuas-MacBook-Air-2.local>
2020-08-02 18:59:22 -0700

    Updated queries and mutations

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,23 +1,5 @@
 import gql from 'graphql-tag';
 
-export const QUERY_THOUGHTS = gql`
-  query thoughts($username: String) {
-    thoughts(username: $username) {
-      _id
-      thoughtText
-      createdAt
-      username
-      reactionCount
-      reactions {
-        _id
-        createdAt
-        username
-        reactionBody
-      }
-    }
-  }
-`;
-
 export const GET_ME = gql`
 {
     me {
